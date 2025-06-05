73220acc4c339475c278950c22cb5fd0940c74ac
Miriam Sanchez <mirisaor02@gmail.com>
2025-06-05 10:22:32 +0200

    feature-change pointer and slider

---
+++ src/assets/data/sliderData.js
@@ -0,0 +1,30 @@
+import Andaman from '../images/Andaman.jpg';
+import boreal from '../images/boreal.jpg';
+import bison from '../images/bison.jpg';
+import bosc from '../images/Bosc.jpg';
+import viajar from '../images/Viajar.jpg';
+
+const sliderData = [
+  {
+    img: Andaman,
+    text: 'Flymily, donde despegan los recuerdos en familia',
+  },
+  {
+    img: boreal,
+    text: 'Mil lugares que descubrir, un planeta que querer',
+  },
+  {
+    img: bison,
+    text: 'La familia debe ser un aliciente para viajar, no un impedimento',
+  },
+  {
+    img: bosc,
+    text: 'Tu próxima aventura, a un solo clic',
+  },
+  {
+    img: viajar,
+    text: 'Viajar juntos nos acerca más que nunca',
+  },
+];
+
+export default sliderData;
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -2,12 +2,8 @@ import React from 'react';
 import Slider from 'react-slick';
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
+import sliderData from '../../assets/data/sliderData.js';
 
-import Andaman from '../../assets/Andaman.jpg';
-import boreal from '../../assets/boreal.jpg'; 
-import bison from '../../assets/bison.jpg';
-import bosc from '../../assets/Bosc.jpg';
-import viajar from '../../assets/Viajar.jpg';
 
 import styles from './Home.module.css';
 
@@ -21,35 +17,16 @@ const Home = () => {
     pauseOnHover: false,
   };
 
-  const slides = [
-    {
-      img: Andaman,
-      text: 'Flymily, donde despegan los recuerdos en familia',
-    },
-    {
-      img: boreal,
-      text: 'Mil lugares que descubrir, un planeta que querer',
-    },
-    {
-      img: bison,
-      text: 'La familia debe ser un aliciente para viajar, no un impedimento',
-    },
-    {
-      img: bosc,
-      text: 'Tu próxima aventura, a un solo clic',
-    },
-    {
-      img: viajar,
-      text:'Viajar juntos nos acerca más que nunca',
-    },
-  ];
-
   return (
     <div className={styles.sliderContainer}>
       <Slider {...settings}>
-        {slides.map((slide, index) => (
+        {sliderData.map((slide, index) => (
           <div key={index} className={styles.slide}>
-          <img src={slide.img} alt={`Imagen ${index + 1}`} className={styles.slideImg} />
+          <img 
+            src={slide.img} 
+            alt={`Imagen ${index + 1}`} 
+            className={styles.slideImg} 
+          />
           <div className={styles.textBackdrop}>
             <h2 className={styles.centeredText}>{slide.text}</h2>
           </div>
