d46835852d078170c6bda85b85de0132a47bdada
Miriam Sanchez <mirisaor02@gmail.com>
2025-06-10 09:51:42 +0200

    footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { Routes, Route } from 'react-router-dom';
 import Navbar from './components/navbar/Navbar';
+import Footer from './components/footer/Footer';
 
 import Home from './pages/home/Home';
 import Viajes from './pages/viajes/Viajes';
