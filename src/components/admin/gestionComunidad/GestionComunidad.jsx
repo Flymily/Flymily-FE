85187a90e9cf241db4b01cad1f61942516da80e3
Israel Espin <israel.espin92@gmail.com>
2025-06-20 20:45:43 +0200

    add comunity panel admin

--- src/components/admin/gestionComunidad/GestionComunidad.jsx
+++ src/components/admin/gestionComunidad/GestionComunidad.jsx
@@ -1,61 +1,23 @@
-import { useEffect, useState } from "react";
-import styles from "./GestionComunidad.module.css";
-import { getContenidoComunidad, updateContenidoComunidad } from '../../../services/comunidad';
+import { useState } from 'react';
+import ComunidadFormulario from './ComunidadFormulario';
+import ComunidadLista from './ComunidadLista';
+import styles from './GestionComunidad.module.css';
 
 const GestionComunidad = () => {
-  const [contenido, setContenido] = useState("");
-  const [loading, setLoading] = useState(true);
-  const [guardado, setGuardado] = useState(false);
+  const [postEditando, setPostEditando] = useState(null);
+  const [reload, setReload] = useState(false);
 
-  useEffect(() => {
-    const fetchContenido = async () => {
-      try {
-        const response = await getContenidoComunidad();
-        console.log("➡️ Contenido recibido:", response.data);
-        setContenido(response.data.contenido);
-      } catch (error) {
-        console.error("❌ Error al cargar contenido:", error);
-      } finally {
-        setLoading(false);
-      }
-    };
-
-    fetchContenido();
-  }, []);
-
-  const handleChange = (e) => {
-    setContenido(e.target.value);
-  };
-
-  const handleSave = async () => {
-    try {
-      await updateContenidoComunidad(contenido);
-      setGuardado(true);
-      setTimeout(() => setGuardado(false), 2000);
-    } catch (error) {
-      console.error("❌ Error al guardar contenido:", error);
-    }
+  const manejarRecarga = () => {
+    setReload(!reload);
+    setPostEditando(null);
   };
 
   return (
-    <div className={styles.wrapper}>
-      <h3>Editar título o contenido de la comunidad</h3>
-      {loading ? (
-        <p>Cargando...</p>
-      ) : (
-        <>
-          <textarea
-            value={contenido}
-            onChange={handleChange}
-            rows={6}
-            className={styles.textarea}
-          />
-          <button onClick={handleSave} className={styles.botonGuardar}>
-            Guardar cambios
-          </button>
-          {guardado && <p className={styles.guardado}>¡Contenido guardado!</p>}
-        </>
-      )}
+    <div className={styles.panel}>
+      <div className={styles.contenido}>
+        <ComunidadFormulario postEditando={postEditando} onSuccess={manejarRecarga} />
+        <ComunidadLista onEdit={setPostEditando} reload={reload} esAdmin={true} />
+      </div>
     </div>
   );
 };
