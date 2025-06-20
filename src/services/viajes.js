94ca37cdb664c5a16bc0f00089b4686f8d0119b6
Miriam Sanchez <mirisaor02@gmail.com>
2025-06-20 19:09:14 +0200

    change viajes.js

--- src/services/viajes.js
+++ src/services/viajes.js
@@ -2,7 +2,7 @@ import { api } from './api';
 import { apiProtected } from './apiProtected';
 
 export const getAllViajesPublicos = () =>api.get('/viajes/filtrar/detalle/all');
-export const getAllViajes        = () => api.get('/viajes/filtrar/detalle/all');
+export const getAllViajes = () => apiProtected.get('/viajes/filtrar/detalle/all');
 
 
 export const createViaje = body => apiProtected.post('/viajes/crear', body);
